547a21506e766ce401dab21e83e0a0f6e18b14db
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-05-25 08:19:02 +0400

    Написал алгоритм поиска изображений

--- FindFiles/Program.cs
+++ FindFiles/Program.cs
@@ -4,15 +4,16 @@ namespace FindFiles;
 
 class Program
 {
-	private static string[] fileTypes = new[] { ".png", ".gif", ".jpg", ".jpeg", ".dll" };
+	private static string[] fileTypes = new[] { ".png", ".gif", ".jpg", ".jpeg" };
 	static void Main(string[] args)
 	{
+		List<string> files = new List<string>();
 		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 		{
 			DriveInfo[] allDrives = DriveInfo.GetDrives();
 			foreach (var drive in allDrives)
 			{
-				FindFiles(drive.Name);
+				FindFiles(drive.Name, files);
 			}
 		}
 
@@ -23,9 +24,28 @@ class Program
 		}
 	}
 
-	public static void FindFiles(string currentDirectory)
+	public static void FindFiles(string currentDirectory, List<string> files)
 	{
-		Directory.SetCurrentDirectory(currentDirectory);
-		Console.WriteLine(Directory.GetCurrentDirectory());
+		try
+		{
+			var filesInDirectory = Directory.GetFiles(currentDirectory).ToList();
+			if (filesInDirectory.Count > 0)
+			{
+				foreach (var fileType in fileTypes)
+				{
+					files.AddRange(filesInDirectory.Where(e=>e.Contains(fileType)));
+				}
+			}
+			var directories = Directory.GetDirectories(currentDirectory);
+			if (directories.Length < 1) return;
+			foreach (var directory in directories)
+			{
+				FindFiles(directory, files);
+			}
+			// Console.WriteLine(Directory.GetCurrentDirectory());
+		}
+		catch (Exception e)
+		{
+		}
 	}
 }
